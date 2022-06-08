99716c7ad318f248655d4fff1bb6eec19b820a54
benjaminluan <benjaminluan1@gmail.com>
2022-06-08 18:14:18 -0400

    Reworked project

--- src/projects.js
+++ src/projects.js
@@ -1,30 +1,50 @@
 export const projects = [
-    {
-      id: 1,
-      title: "League of Legends Champion Info",
-      img:"./images/League.png",
-      source:"https://github.com/benjaminluan/League-react",
-      preview: "",
-      para: "A league of Legends fan-made project that can filter and showcase champions description",
-      language: "",
-    },
-    {
-      id:2,
-      title:"Library Project",
-      img:"./images/Library.png",
-      source:"https://github.com/benjaminluan/Library-Project.git",
-      preview: "https://benjaminluan.github.io/Library-Project/",
-      para: "A project with the intention to grasp a better understanding of HTML, CSS and Javascript",
-      language: "HTML, CSS, Javascript",
-    },
+  {
+    id: 1,
+    title: "League of Legends Champion Info",
+    img: "./images/League.png",
+    source: "https://github.com/benjaminluan/League-react",
+    preview: "",
+    para: "A league of Legends fan-made project that can filter and showcase champions description.",
+    language: "",
+  },
+  {
+    id: 2,
+    title: "Library Project",
+    img: "./images/Library.png",
+    source: "https://github.com/benjaminluan/Library-Project.git",
+    preview: "https://benjaminluan.github.io/Library-Project/",
+    para: "A project with the intention to grasp a better understanding of HTML, CSS and Javascript.",
+    language: "HTML, CSS, Javascript",
+  },
 
-    {
-      id:3,
-      title:"First Portfolio",
-      img:"",
-      source:"https://github.com/benjaminluan/Library-Project.git",
-      preview: "https://benjaminluan.github.io/Library-Project/",
-      para: "A project with the intention to grasp a better understanding of HTML, CSS and Javascript",
-      language: "HTML, CSS, Javascript",
-    }
-]
+  {
+    id: 3,
+    title: "First Portfolio",
+    img: "./images/portfolio.png",
+    source: "https://github.com/benjaminluan/Library-Project.git",
+    preview: "https://benjaminluan.github.io/Library-Project/",
+    para: "A project with the intention to grasp a better understanding of HTML, CSS and Javascript.",
+    language: "HTML, CSS, Javascript",
+  },
+
+  {
+    id: 4,
+    title: "First Portfolio",
+    img: "./images/portfolio.png",
+    source: "https://github.com/benjaminluan/Library-Project.git",
+    preview: "https://benjaminluan.github.io/Library-Project/",
+    para: "A project with the intention to grasp a better understanding of HTML, CSS and Javascript.",
+    language: "HTML, CSS, Javascript",
+  },
+  
+  {
+    id: 5,
+    title: "First Portfolio",
+    img: "./images/portfolio.png",
+    source: "https://github.com/benjaminluan/Library-Project.git",
+    preview: "https://benjaminluan.github.io/Library-Project/",
+    para: "A project with the intention to grasp a better understanding of HTML, CSS and Javascript.",
+    language: "HTML, CSS, Javascript",
+  },
+];
