6f56cbb80baefde2c4830cd4ae75b538b516ac3d
benjaminluan <benjaminluan1@gmail.com>
2022-07-07 11:36:55 -0400

    Project Img rescaling

--- src/projects.js
+++ src/projects.js
@@ -1,12 +1,12 @@
 export const projects = [
   {
     id: 1,
-    title: "LoL Info",
-    img: "./images/league.png",
-    source: "https://github.com/benjaminluan/League-react",
-    preview: "https://league-react.vercel.app/",
-    para: "A league of Legends fan-made project that can filter and showcase champions description.",
-    language: "HTML, CSS, Javascript, React",
+    title: "1:1 Treact Template",
+    img: "./images/treact.png",
+    source: "https://github.com/benjaminluan/Treact-Project",
+    preview: "https://benjaminluan.github.io/Treact-Project/",
+    para: "A project with the intention to grasp a better understanding of HTML, CSS and Javascript.",
+    language: "HTML, CSS ",
   },
   {
     id: 2,
@@ -27,14 +27,31 @@ export const projects = [
     para: "A project with the intention to grasp a better understanding of HTML, CSS and Javascript.",
     language: "HTML, CSS, Javascript",
   },
-
   {
     id: 4,
-    title: "1:1 Treact Template",
-    img: "./images/treact.png",
-    source: "https://github.com/benjaminluan/Treact-Project",
-    preview: "https://benjaminluan.github.io/Treact-Project/",
-    para: "A project with the intention to grasp a better understanding of HTML, CSS and Javascript.",
-    language: "HTML, CSS ",
-  }
+    title: "LoL Info",
+    img: "./images/league.png",
+    source: "https://github.com/benjaminluan/League-react",
+    preview: "https://league-react.vercel.app/",
+    para: "A league of Legends fan-made project that can filter and showcase champions description.",
+    language: "HTML, CSS, Javascript, React",
+  },
+  {
+    id: 5,
+    title: "Spotify Clone",
+    img: "./images/spotify.png",
+    source: "https://github.com/benjaminluan/Spotify-Clone",
+    preview: "https://spotify-clone-lovat-phi.vercel.app/",
+    para: "Spotify Clone",
+    language: "HTML, CSS, Javascript, React",
+  },
+  {
+    id: 6,
+    title: "Search Engine",
+    img: "./images/google.png",
+    source: "https://github.com/benjaminluan/Google-Clone",
+    preview: "https://google-clone-liart-delta.vercel.app/",
+    para: "Google Clone",
+    language: "HTML, CSS, Javascript, React",
+  },
 ];
